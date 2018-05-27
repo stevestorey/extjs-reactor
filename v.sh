5109a4e7df39035af95f95722d8cbb87ea92fc6f
mgusmano <mgusmano@yahoo.com>
2018-05-27 11:57:12 -0400

    v70

---
+++ v.sh
@@ -0,0 +1,15 @@
+cd packages
+cd ext-build
+npm version patch
+
+cd ..
+cd ext-build-generate-app
+npm version patch
+
+cd ..
+cd ext-gen
+npm version patch
+
+cd ..
+cd ext-webpack-plugin
+npm version patch
